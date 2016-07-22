ffe328cc48c77caa4c389857cf5091ad85c8de60
Aravind Raj <aravindcraj@gmail.com>
2016-07-22 23:51:24 +0530

    Task 2: Getting inputs.

---
+++ input.py
@@ -0,0 +1,21 @@
+name = raw_input("Enter your name: ")
+
+print "Hello, ", name
+
+print "Integer inputs"
+
+a = int(raw_input("Enter a value: "))
+b = int(raw_input("Enter b value: "))
+
+c = a + b
+
+print "Sum: ", c
+
+print "Float inputs"
+
+d = float(raw_input("Enter d float value: "))
+e = float(raw_input("Enter e float value: "))
+
+f = d + e
+
+print "Float sum: ", f
