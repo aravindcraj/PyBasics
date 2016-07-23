aac2b7f181a0181808781a6568021b64c8ce6078
Aravind Raj <aravindcraj@gmail.com>
2016-07-23 23:52:45 +0530

    Task 3: Bit wise operations. Task 4: Comparison operations.

---
+++ bitwise.py
@@ -0,0 +1,20 @@
+a = 60
+b = 13
+
+c = a & b
+print "Value of C is: ", c
+
+c = a | b
+print "Value of C is: ", c
+
+c = a ^ b
+print "Value of C is: ", c
+
+c = -a
+print "Value of C is: ", c
+
+c = a << 2
+print "Value of C is: ", c
+
+c = a >> 2
+print "Value of C is: ", c
---
+++ comparision.py
@@ -0,0 +1,38 @@
+a = int(input("Enter a value: "))
+b = int(input("Enter b value: "))
+
+if a == b:
+    print "A is equals to B"
+
+else:
+    print "A is not equal to B"
+
+if b != a:
+    print "B is not equal to B"
+
+else:
+    print "A is equal to B"
+
+if a < b:
+    print "A is lesser than B"
+
+else:
+    print "A is not lesser than B"
+
+if a > b:
+    print "A is greater than B"
+
+else:
+    print "B is greater than A"
+
+if a <= b:
+    print "A is either lesser or equal to B"
+
+else:
+    print "A is neither less nor greater to B"
+
+if a >= b:
+    print "A is either greater or equal to B"
+
+else:
+    print "A is neither greater nor equal to B"
